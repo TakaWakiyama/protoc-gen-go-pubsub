e7ce5abeff7a9c327cae183f25b8e38100153c33
TAKA <twakiyama.biz@gmail.com>
2023-05-08 20:36:30 +0900

    fix: mount方法を修正

--- example/generated/sub.pb.go
+++ example/generated/sub.pb.go
@@ -7,7 +7,6 @@
 package example
 
 import (
-	_ "github.com/TakaWakiyama/protoc-gen-go-pubsub/example/option"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
